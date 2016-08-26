e3dfd61c74069826391ea047e831c57684c42211
Alyssa Hursh <akhursh@gmail.com>
2016-08-26 15:19:14 -0700

    Functioning song.

--- 02-12Days/12days_test.rb
+++ 02-12Days/12days_test.rb
@@ -11,7 +11,6 @@ class TwelveDaysTest < Minitest::Test
   end
 
   def test_day_two_gift
-    skip
     l1 = "two turtle doves"
     l2 = "and a partridge in a pear tree"
     g1, g2 = Xmas.gifts(1)
@@ -21,20 +20,17 @@ class TwelveDaysTest < Minitest::Test
   end
 
   def test_day_five_yelling_gift
-    skip
     yell = Xmas.gifts(4).first
     assert_equal "FIVE GOLDEN RINGS", yell
   end
 
   def test_first_verse
-    skip
     v1, v2 = Xmas.verse(0).split("\n")
     assert_equal "On the first day of Christmas my true love gave to me", v1
     assert_equal "a partridge in a pear tree", v2
   end
 
   def test_second_verse
-    skip
     v1, v2, v3 = Xmas.verse(1).split("\n")
 
     assert_equal "On the second day of Christmas my true love gave to me", v1
@@ -43,7 +39,6 @@ class TwelveDaysTest < Minitest::Test
   end
 
   def test_last_verse
-    skip
     last_verse = <<-SONG
 On the twelfth day of Christmas my true love gave to me
 twelve drummers drumming
@@ -64,8 +59,102 @@ SONG
   end
 
   def test_sing_whole_song
-    skip
     whole_song = File.read(File.dirname(__FILE__) + '/12days.txt')
     assert_equal whole_song.strip, Xmas.sing
   end
+end
+
+class Xmas
+
+  attr_accessor :gifts, :verse
+
+  def self.gifts(day)
+    day12 = "twelve drummers drumming"
+    day11 = "eleven pipers piping"
+    day10 = "ten lords-a-leaping"
+    day09 = "nine ladies dancing"
+    day08 = "eight maids-a-milking"
+    day07 = "seven swans-a-swimming"
+    day06 = "six geese-a-laying"
+    day05 = "FIVE GOLDEN RINGS"
+    day04 = "four calling birds"
+    day03 = "three french hens"
+    day02 = "two turtle doves"
+    day01 = "and a partridge in a pear tree"
+    day00 = "a partridge in a pear tree"
+
+    case day
+    when 0
+      [day00]
+    when 1
+      [day02, day01]
+    when 2
+      [day03, day02, day01]
+    when 3
+      [day04, day03, day02, day01]
+    when 4
+      [day05, day04, day03, day02, day01]
+    when 5
+      [day06, day05, day04, day03, day02, day01]
+    when 6
+      [day07, day06, day05, day04, day03, day02, day01]
+    when 7
+      [day08, day07, day06, day05, day04, day03, day02, day01]
+    when 8
+      [day09, day08, day07, day06, day05, day04, day03, day02, day01]
+    when 9
+      [day10, day09, day08, day07, day06, day05, day04, day03, day02, day01]
+    when 10
+      [day11, day10, day09, day08, day07, day06, day05, day04, day03, day02, day01]
+    when 11
+      [day12, day11, day10, day09, day08, day07, day06, day05, day04, day03, day02, day01]
+    end
+  end
+
+  def self.verse(verse_input)
+    lines_to_sing = ""
+    self.gifts(verse_input).each do |line|
+      lines_to_sing += "\n#{line}"
+    end
+
+    case verse_input
+    when 0
+      "On the first day of Christmas my true love gave to me\n#{self.gifts(0).first}"
+    when 1
+      "On the second day of Christmas my true love gave to me#{lines_to_sing}"
+    when 2
+      "On the third day of Christmas my true love gave to me#{lines_to_sing}"
+    when 3
+      "On the fourth day of Christmas my true love gave to me#{lines_to_sing}"
+    when 4
+      "On the fifth day of Christmas my true love gave to me#{lines_to_sing}"
+    when 5
+      "On the sixth day of Christmas my true love gave to me#{lines_to_sing}"
+    when 6
+      "On the seventh day of Christmas my true love gave to me#{lines_to_sing}"
+    when 7
+      "On the eighth day of Christmas my true love gave to me#{lines_to_sing}"
+    when 8
+      "On the ninth day of Christmas my true love gave to me#{lines_to_sing}"
+    when 9
+      "On the tenth day of Christmas my true love gave to me#{lines_to_sing}"
+    when 10
+      "On the eleventh day of Christmas my true love gave to me#{lines_to_sing}"
+    when 11
+      "On the twelfth day of Christmas my true love gave to me#{lines_to_sing}"
+    end
+  end
+
+  def self.sing
+    song = "#{verse(0)}"
+    counter = 1
+    11.times do
+      song += "\n\n#{verse(counter)}"
+      counter += 1
+    end
+
+    song
+
+  end
+
 end
